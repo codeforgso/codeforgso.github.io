dba758b3938b108ee23557016c6035b800260996
Rob Martin <rob.martin.5758@gmail.com>
2019-09-12 22:17:57 -0400

    Join Us button fixes

--- components/page.js
+++ components/page.js
@@ -2,22 +2,20 @@ import React from 'react';
 import Head from '../components/head';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
-import Join from '../components/join';
+import JoinButton from './join-button';
 import '../styles.scss';
 
 const Page = ({ children, title = '' }) => (
   <React.Fragment>
     <Head title={title} />
     <Nav />
-    <Join location='top'/>
+    <JoinButton location='top'/>
     <div className="section">
       <div className="container">{children}</div>
     </div>
-    <Join location='bottom'/>
+    <JoinButton location='bottom'/>
    <Footer />
   </React.Fragment>
-
-   
 );
 
 export default Page;
