97c7e0e071e9109ae1b13a36f0ef0eeb33055f27
Braden Douglass <braden.douglass@gmail.com>
2019-09-21 15:15:56 -0400

    Migrate Lorem IPSUM In About Page To Old Content. (#15)
    * Migrate Lorem IPSUM To Old Content.
    
    Merge in content from the older code for greensobor RAILS site. Some
    link to helpers sticking around but for the most part.
    
    Co-authored-by: John Hill <john.m.hill91@gmail.com>
    
    * Move Link To Helpers To Actual A Tags.
    
    Next has some wonderful helper link tags but, none of these URL's are
    internal to the application.
    
    Co-authored-by: John Hill <john.m.hill91@gmail.com>
    
    * Move To ClassName Over Class
    
    The copy pasta of the older rails version does support the actual dom
    API. Good for you React. Move to ClassName over Class to make the next
    CLI happy.

--- pages/about.js
+++ pages/about.js
@@ -5,19 +5,33 @@ import '../styles.scss';
 const About = () => (
   <Page title="About" >
     <p className="title" >About</p>
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <div className="content">
+      <div className="leftcolumn">
+        <p>
+          The Code for America mission is, in part, to
+          <blockquote>build open source technology and organize a network of people dedicated to making government services simple, effective, and easy to use.</blockquote>
+        </p>
+
+        <p>
+          As a Code for America Brigade, CfG extends this mission to the government and citizens of Greensboro. Our mission is to improve access to city data, to assist in creating and bettering city websites—and generally making the relationship between the city and the citizens better through technology.
+        </p>
+
+        <h5>For Example</h5>
+
+        <p>What sorts of projects do we do? Mapping and recording the status of fire hydrants. Tracking storm water data. Building apps that report trash collection times and let citizens report missed pickups. In short, the projects we undertake improve the parts of city government that would be "nice to have", but that—in the course of running an entire city—can sometimes be forgotten.</p>
+
+        <h5>Is This Just For Coders?</h5>
+
+        <p>The short answer is, "nope". We need people from all walks of life to participate. Of course we need developers, designers, user experience and user interface types, and project managers. But you may not know that we also need urban planners, software testers, photographers, bloggers, product managers, and anyone else who wants to make technology access better in the Gate City. As we always say, we need the <em>community</em>.</p>
+
+        <h5>What goes on at our meetups?</h5>
+
+        <p>We start every meetup with introductions and a welcome to new members. From there we move into a presentation or updates from members who have attended community events, meetings, or anything else that would interest the Brigade. After that, our Project Leads will give quick project status updates and let everyone know what they need help with that night - and then the work begins.</p>
+
+        <p>Have an idea for a presentation you'd like to hear? Want to sign up to give one? Add to the list or assign a topic to yourself <a href="https://github.com/codeforgso/lightning-talks/issues">here</a>. Not a member of our GitHub organization? Let us know by jumping into Slack posting to the general channel.  We hope to see you at our next <a href="http://www.meetup.com/Code-for-Greensboro/"> Meetup</a>!</p>
+      </div>
     </div>
   </Page>
-);
+  );
 
-export default About;
+  export default About;
