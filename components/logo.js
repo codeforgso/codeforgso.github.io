2d14040469bba8eb094c3ec22dfdd98369760100
Rob Martin <Rob.martin.5758@gmail.com>
2019-09-24 08:59:44 -0400

    #3 Logo component and standardize classNames/styles (#18)
    * 2 options for footer: sticky bottom or end content
    
    * 2 options for footer: sticky bottom or end content
    
    * 2 options for footer: sticky bottom or end content
    
    * increave viewport over 100%/vh to force scroll
    
    * remove extraneous code added
    
    * nav bar links and initial link pages
    
    * remove invalid character
    
    * join us button updates
    
    * Join Us button fixes
    
    * logo component (allow reuse), standardize styles

---
+++ components/logo.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Logo = () => {
+    return (
+        <Link href="/">
+            <a className="navbar-item">
+                <div className="Logo">
+                    <img src="/static/cfg_logo.png" />
+                    <div className="textContainer">
+                        <p className="topText">Code for</p>
+                        <p className="bottomText">Greensboro</p>
+                    </div>
+                </div>
+            </a>
+        </Link>
+    );
+}
+
+export default Logo;
