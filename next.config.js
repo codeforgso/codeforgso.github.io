adc37a4cd4fd6c0088ef4d3ca1b1e670e5edd121
wanderlust <wander-igdst@gmail.com>
2019-10-06 22:17:22 +0530

    Added image to home page & about page

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
-const withSass = require('@zeit/next-sass')
+const withSass = require("@zeit/next-sass");
 
-module.exports = withSass( {
+module.exports = withSass({
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: 'empty'
-    }
-    return config
+      fs: "empty"
+    };
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
   }
-})
+});
