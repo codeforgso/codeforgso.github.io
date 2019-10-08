4184a5560168bd2efb89141294a2db15051bc3a9
wander-igdst <56194378+wander-igdst@users.noreply.github.com>
2019-10-08 18:29:26 +0530

    Added image to home page & about page (#33)
    * Added image to home page & about page
    
    * Removing webpack loader
    
    * Using png format for image

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import Page from '../components/page';
-import '../styles.scss';
-
+import React from "react";
+import Page from "../components/page";
+import "../styles.scss";
 const Home = () => (
-  <Page title="Home" >
-    <p className="title" >Home</p>
+  <Page title="Home">
+    <p className="title">Home</p>
     <div className="content">
       <p className="subtitle">
-        Code for Greensboro works to improve the way our community uses technology and data. And we need you—no coding required.
-        </p>
+        Code for Greensboro works to improve the way our community uses
+        technology and data. And we need you—no coding required.
+      </p>
+    </div>
+    <div className="img-wrapper">
+      <img
+        src="../static/Washington-DC-Capitol.png"
+        alt="Washington DC Capitol"
+        style={{
+          width: "100%"
+        }}
+      />
+    </div>
+    <div>
       <div className="container">
-        Code for Greensboro is a local chapter of Code for America, a national 501(c)(3) nonprofit. We are dedicated to helping grow Greensboro’s tech community and improve local government services through the development of civic technology projects, the volunteer efforts of local members of our community, and by educating the public on opportunities to contribute to their community through technology.
+        Code for Greensboro is a local chapter of Code for America, a national
+        501(c)(3) nonprofit. We are dedicated to helping grow Greensboro’s tech
+        community and improve local government services through the development
+        of civic technology projects, the volunteer efforts of local members of
+        our community, and by educating the public on opportunities to
+        contribute to their community through technology.
       </div>
       <div className="container">
-        Code for Greensboro could not make it without the help of its community. Both tech-savy and none, we have had the opportunity to help others through the projects we conduct. The more, the merrier, as the level of impact we have is dependant on those who help. Join us on Slack and our meetups.
+        Code for Greensboro could not make it without the help of its community.
+        Both tech-savy and none, we have had the opportunity to help others
+        through the projects we conduct. The more, the merrier, as the level of
+        impact we have is dependant on those who help. Join us on Slack and our
+        meetups.
       </div>
     </div>
   </Page>
