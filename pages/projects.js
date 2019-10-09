ba91ad7d8d9a728e9fb159a0d141457d1d170b88
Schaffer Stewart <Schaffer.Stewart@ibm.com>
2019-10-08 20:56:01 -0400

    eslint fixes

--- pages/projects.js
+++ pages/projects.js
@@ -3,40 +3,38 @@ import Page from '../components/page'
 import '../styles.scss'
 
 const Projects = () => (
-<Page title="Our Projects">
+    <Page title="Our Projects">
         <div>
             <h2>Re-entry Resources</h2>
-            <div className='projects'>
-               <p> A website providing information relevant and valuable to
-                residents reentering communities after a period of
-                incarceration. Project initiated by Code for Asheville and
-                scaled by Open NC.</p>
-                {' '}
+            <div className="projects">
                 <p>
-                  <a href="https://github.com/Open-NC/reentry-resources-hub">
                     {' '}
-Re-entry Resources Repository (Open NC){' '}
-                  </a>
+                    A website providing information relevant and valuable to
+                    residents reentering communities after a period of
+                    incarceration. Project initiated by Code for Asheville and
+                    scaled by Open NC.
+                </p>{' '}
+                <p>
+                    <a href="https://github.com/Open-NC/reentry-resources-hub">
+                        {' '}
+                        Re-entry Resources Repository (Open NC){' '}
+                    </a>
                 </p>
-            </div> 
-            <br/>
+            </div>
+            <br />
             <h2>GoVoteGSO</h2>
-            <div className='projects'>
+            <div className="projects">
                 A web app providing local residents the ability to look up voter
                 registration status and polling place information by address.
                 Project initiated in partnership with the League of Women{"'"}s
-                Voters Triad.{' '}
-              <br/>
-              <a href="https://github.com/codeforgso/GoVote">
-                GoVoteGSO Repository
-              </a>
+                Voters Triad. <br />
+                <a href="https://github.com/codeforgso/GoVote">
+                    GoVoteGSO Repository
+                </a>
             </div>
             <div>
                 {' '}
-                <a href="https://govotegso.org/">
-                    {' '}
-                    Open NC Repository{' '}
-                </a>
+                <a href="https://govotegso.org/"> Open NC Repository </a>
             </div>
         </div>
     </Page>
