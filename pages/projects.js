705d0658889e64d0d600b6d4f69cc4fbdb6bc1ad
tleisman <timleisman@outlook.com>
2019-10-08 20:35:56 -0400

    forgot to stage changes on project page

--- pages/projects.js
+++ pages/projects.js
@@ -6,25 +6,30 @@ const Projects = () => (
 <Page title="Our Projects">
         <div>
             <h2>Re-entry Resources</h2>
-            <div>
-                A website providing information relevant and valuable to
+            <div className='projects'>
+               <p> A website providing information relevant and valuable to
                 residents reentering communities after a period of
                 incarceration. Project initiated by Code for Asheville and
-                scaled by Open NC.
-            </div>
-            <div>
+                scaled by Open NC.</p>
                 {' '}
-                <a href="https://github.com/Open-NC/reentry-resources-hub">
+                <p>
+                  <a href="https://github.com/Open-NC/reentry-resources-hub">
                     {' '}
-                    Open NC Repository{' '}
-                </a>
-            </div>
+Re-entry Resources Repository (Open NC){' '}
+                  </a>
+                </p>
+            </div> 
+            <br/>
             <h2>GoVoteGSO</h2>
-            <div>
+            <div className='projects'>
                 A web app providing local residents the ability to look up voter
                 registration status and polling place information by address.
                 Project initiated in partnership with the League of Women's
                 Voters Triad.{' '}
+              <br/>
+              <a href="https://github.com/codeforgso/GoVote">
+                GoVoteGSO Repository
+              </a>
             </div>
         </div>
     </Page>
