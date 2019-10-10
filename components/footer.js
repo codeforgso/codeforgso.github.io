08cc8dbb78fcc59a3e933a629f1b5436a185fb43
John Hill <john.m.hill91@gmail.com>
2019-10-09 20:10:00 -0400

    Master (#28)
    * played with
    
    * worked on join us page
    
    * worked on footer
    
    * set up comment box still working on submit

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,41 @@
+
+const links = [
+  { href: '/join', label: 'Join Us' },
+  { href: '/resources', label: 'Resources' },
+  { href: '/contact', label: 'Contact' },
+  { href: '/conduct', label: 'Code of Conduct' },
+].map((link) => {
+  link.key = `nav-link-${link.href}-${link.label}`;
+  return link;
+});
+
 const footer = () => (
     <footer className="footer">
         <div className="columns">
             <div className="column">
                 <div className="title is-4 red has-text-centered">
-                    <h1>footer text</h1>
+                    <h1>Join</h1>
                 </div>
             </div>
             <div className="column">
                 <div className="title is-4 red has-text-centered">
-                    <h1>footer text</h1>
+                    <h1>Code of Conduct</h1>
                 </div>
             </div>
             <div className="column">
                 <div className="title is-4 red has-text-centered">
-                    <h1>footer text</h1>
+                    <h1>Resources</h1>
                 </div>
+            </div>
+            <div className="column">
+                <div className="title is-4 red has-text-centered">
+                    <h1>Contact Us</h1>
+                </div>
+            </div>
+          <div class="columns">
+            <div class="column">
+                Copyright {new Date().getFullYear()} Code For Greensboro
+            </div>
             </div>
         </div>
     </footer>
