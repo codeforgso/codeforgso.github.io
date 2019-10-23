6dd8d8925be29a2ff1b55dfe77cc48f55717c986
Akanksha Jai <akankshajai20@gmail.com>
2019-10-23 19:58:06 +0530

    Link To Footer Done

--- components/footer.js
+++ components/footer.js
@@ -1,24 +1,34 @@
+import Link from 'next/link'
+
 const footer = () => (
     <footer className="footer">
         <div className="columns">
             <div className="column">
                 <div className="title is-4 red has-text-centered">
-                    <h1>Join</h1>
+                    <Link href="/join">
+                        <h1>Join</h1>
+                    </Link>
                 </div>
             </div>
             <div className="column">
                 <div className="title is-4 red has-text-centered">
-                    <h1>Code of Conduct</h1>
+                    <Link href="/conduct">
+                        <h1>Code of Conduct</h1>
+                    </Link>
                 </div>
             </div>
             <div className="column">
                 <div className="title is-4 red has-text-centered">
-                    <h1>Resources</h1>
+                    <Link href="/resources">
+                        <h1>Resources</h1>
+                    </Link>
                 </div>
             </div>
             <div className="column">
                 <div className="title is-4 red has-text-centered">
-                    <h1>Contact Us</h1>
+                    <Link href="/contact">
+                        <h1>Contact Us</h1>
+                    </Link>
                 </div>
             </div>
             <div className="columns">
