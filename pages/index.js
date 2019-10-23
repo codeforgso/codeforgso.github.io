d30c01e64f8e0b1afcce62b525f8834e74ec8721
Rob Martin <rob.martin.5758@gmail.com>
2019-10-23 14:58:54 -0400

    Make clear that CfG is not just for Coders

--- pages/index.js
+++ pages/index.js
@@ -4,34 +4,36 @@ import HeroImage from '../components/hero-image'
 
 const Home = () => (
     <Page title="Home">
-        <div className="content Home">
+        <div className="Home container content">
             <p className="subtitle">
-                Code for Greensboro works to improve the way our community uses
-                technology and data. And we need you—no coding required.
+                {/* Code for Greensboro works to improve the way our community uses
+                technology and data. And we need you—no coding required. */}
+                Code for Greensboro is a welcoming and inclusive volunteer group
+                of civic-minded thinkers, problem solvers, designers and techies
+                who use creative technology to solve civic and social problems.
             </p>
-
             <HeroImage
                 src="../static/GreensboroSkylineSmall.jpg"
                 alt="Greensboro Skyline"
             />
-            <div>
-                <div className="container">
-                    Code for Greensboro is a local chapter of Code for America,
-                    national 501(c)(3) nonprofit. We are dedicated to helping
-                    Greensboro’s tech community and improve local government
-                    services through the development of civic technology the the
-                    the volunteer efforts of local members of our community, and
-                    educating the public on opportunities to contribute to their
-                    community through technology.
-                </div>
-                <div className="container">
-                    Code for Greensboro could not make it without the help of
-                    community. Both tech-savvy and not, we have had the to help
-                    to help others through the projects we conduct. The more the
-                    merrier, as the level of impact we have is dependent on who
-                    who help. Join us on Slack and at our meetups.
-                </div>
-            </div>
+        </div>
+        <div className="container content">
+            <p>
+                Code for Greensboro is a local chapter of Code for America, a
+                national 501(c)(3) nonprofit. We are dedicated to helping grow
+                Greensboro’s tech community and improve local government
+                services through the development of civic technology projects,
+                the volunteer efforts of local members of our community, and by
+                educating the public on opportunities to contribute to their
+                community through technology.
+            </p>
+            <p>
+                Code for Greensboro could not make it without the help of our
+                community. Both tech-savvy and not, we have had the opportunity
+                to help others through the projects we conduct. The more the
+                merrier, as the level of impact we have is dependent on those
+                who help. Join us on Slack and at our meetups.
+            </p>
         </div>
     </Page>
 )
