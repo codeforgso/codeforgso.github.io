cf6a183e765550c00e004290fdf29751eed08458
Rob Martin <rob.martin.5758@gmail.com>
2019-10-30 12:21:03 -0400

    cards on project page - initial

--- pages/projects.js
+++ pages/projects.js
@@ -4,6 +4,91 @@ import Page from '../components/page'
 const Projects = () => (
     <Page title="">
         <h1 className="title is-1">Our Projects</h1>
+        <div className="columns is-centered">
+            <div className="column is-one-third">
+                <div className="card">
+                    <header className="card-header">
+                        <p className="card-header-title is-centered">
+                            <div className="card-image">
+                                <figure className="image">
+                                    <img src="../static/vote.jpeg" alt="Vote" />
+                                </figure>
+                            </div>
+                        </p>
+                    </header>
+                    <div className="card-content">
+                        <div className="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            Phasellus nec iaculis mauris.
+                            <a href="#">@bulmaio</a>. <a href="#">#css</a>{' '}
+                            <a href="#">#responsive</a>
+                            <br />
+                            <time dateTime="2016-1-1">
+                                11:09 PM - 1 Jan 2016
+                            </time>
+                        </div>
+                    </div>
+                    <footer className="card-footer">
+                        <a href="#" className="container card-footer-item">
+                            <p className="content is-right">ppppppppp</p>
+                            <p className="content">qqqqqqqqqq</p>
+                        </a>
+                        <a href="#" className="card-footer-item">
+                            Source
+                        </a>
+                        <a href="#" className="card-footer-item">
+                            Help Needed
+                        </a>
+                    </footer>
+                </div>
+            </div>
+            {/*  */}
+            <div className="column is-one-quarter">
+                <div className="card">
+                    <header className="card-header">
+                        <p className="card-header-title is-centered">
+                            Component
+                        </p>
+                        <a
+                            href="#"
+                            className="card-header-icon"
+                            aria-label="more options"
+                        >
+                            <span className="icon">
+                                <i
+                                    className="fas fa-angle-down"
+                                    aria-hidden="true"
+                                ></i>
+                            </span>
+                        </a>
+                    </header>
+                    <div className="card-content">
+                        <div className="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            Phasellus nec iaculis mauris.
+                            <a href="#">@bulmaio</a>. <a href="#">#css</a>{' '}
+                            <a href="#">#responsive</a>
+                            <br />
+                            <time dateTime="2016-1-1">
+                                11:09 PM - 1 Jan 2016
+                            </time>
+                        </div>
+                    </div>
+                    <footer className="card-footer">
+                        <a href="#" className="card-footer-item">
+                            Save
+                        </a>
+                        <a href="#" className="card-footer-item">
+                            Edit
+                        </a>
+                        <a href="#" className="card-footer-item">
+                            Delete
+                        </a>
+                    </footer>
+                </div>
+            </div>
+        </div>
+
         <div>
             <h2>Re-entry Resources</h2>
             <div className="projects">
@@ -37,7 +122,7 @@ const Projects = () => (
                 <a href="https://govotegso.org/"> Open NC Repository </a>
             </div>
         </div>
-    </Page>
+    </Page >
 )
 
 export default Projects
