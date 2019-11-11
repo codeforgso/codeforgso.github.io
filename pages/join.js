7916d57d28dbbf076368a59045ca3e6d5817744e
Heather Gallop <doyouseeitmyway@gmail.com>
2019-11-11 15:08:15 -0500

    Repair page overflowing container. Remove duplicate H1.
    Remove the combination of hard-coded heights and margins to allow page to flow naturally.  Remove unused H1 tag from page.js to improve SEO performance and accessibility. Format links in join.js to match.

--- pages/join.js
+++ pages/join.js
@@ -27,7 +27,11 @@ const Join = () => (
             </div>
             <br />
             <div className="join-item">
-                <a href="https://bit.ly/cfgso-slack" rel="noopener noreferrer" target="_blank">
+                <a
+                    href="https://bit.ly/cfgso-slack"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
                     <div>
                         <img src="../static/icon_slack.svg" alt="Slack logo" />
                     </div>
@@ -38,7 +42,11 @@ const Join = () => (
                 </p>
                 <p>
                     If you are already a member of our Slack workspace,
-                    <a href="https://cfgso.slack.com" rel="noopener noreferrer" target="_blank">
+                    <a
+                        href="https://cfgso.slack.com"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
                         {' '}
                         visit now.
                     </a>
